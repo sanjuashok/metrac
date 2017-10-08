1aeada475f9fccd599c543beeb457f6c6949e833
Sanju Ashok <srinivas.ashok@live.com>
2017-10-07 20:50:48 -0700

    rename main.py + started using docopt

---
+++ metrac.py
@@ -0,0 +1,55 @@
+"""metrac.
+
+Usage:
+  metrac.py add ([-w] <weight> [-m] <message>)
+  metrac.py (-h | --help)
+  metrac.py --version
+"""
+import json
+import datetime
+import requests
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
+from docopt import docopt
+
+arguments = {}
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='metrac 0.0')
+
+url =  "https://metrac-io.firebaseio.com"
+ext =  "/rest/messages.json"
+
+now = datetime.datetime.now()
+
+# need to check that we're doing an add
+if arguments['add']:
+    # get today's key
+    key = str(now.year) + str(now.month).zfill(2) + str(now.day).zfill(2)
+
+    # format the json we're sending it
+    data = {}
+    details = {}
+
+    details['weight'] = arguments['<weight>']
+    details['message'] = arguments['<message>']
+
+    data[key] = details;
+    json_data = json.dumps(data);
+
+    # Define the required scopes
+    scopes = [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/firebase.database"
+    ]
+
+    # Authenticate a credential with the service account
+    credentials = service_account.Credentials.from_service_account_file(
+        "./secrets/metrac-io-firebase-adminsdk-3o30f-0ea8c2fa69.json", scopes=scopes)
+
+    # Use the credentials object to authenticate a Requests session.
+    authed_session = AuthorizedSession(credentials)
+    response = authed_session.put(url+ext, json_data)
+    print response
+
+    response = authed_session.get(url+ext)
+    print response.content
